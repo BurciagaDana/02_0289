0125e645a17db056326c1cef7109486ac1ab5930
BurciagaDana <23308060610289@cetis61.edu.mx>
2026-04-23 19:13:18 -0600

    ola

--- main.py
+++ main.py
@@ -4,23 +4,26 @@ def main(page: ft.Page):
     page.title = "App con botón"
 
     mensaje = ft.Text("hellooooooo worlddddd !", size=25, color="blue")
+    boton1 = ft.ElevatedButton("no tocar")
 
     def cambiar_texto(e):
-        mensaje.value = "¡que onda noob :V!"
-        mensaje.color = "red"
+        if mensaje.value == "hellooooooo worlddddd !":
+            mensaje.value = "¡que onda noob :V!"
+            mensaje.color = "red"
+            boton1.text = "te dije que no toques"   
+        else:
+            mensaje.value = "hellooooooo worlddddd !"
+            mensaje.color = "blue"
+            boton1.text = "no tocar"       
         page.update()
 
-    boton1 = ft.ElevatedButton("no tocar", on_click=cambiar_texto)
+    boton1.on_click = cambiar_texto
 
-    # ✅ Centrado absoluto en pantalla
     page.add(
         ft.Row(
             [
                 ft.Column(
-                    [
-                        mensaje,
-                        boton1
-                    ],
+                    [mensaje, boton1],
                     alignment=ft.MainAxisAlignment.CENTER,
                     horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                     spacing=20,
